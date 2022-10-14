05fa644f815579832553ae9750a0954192349316
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-10-14 10:12:54 +0100

    Created header component with css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
 import React from "react";
+import "./header.css";
 
 export default function Header() {
-  return <div>Hello from header</div>;
+  return (
+    <div className="Header">
+      <div className="Header-logo">Lauren</div>
+      <div className="Header-menu">
+        <ul>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+        <button>Resume</button>
+      </div>
+    </div>
+  );
 }
